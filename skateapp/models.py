3473fdc36ba0ea7d8fb4e3b4e6391756e9d53a31
vanderpatrick <patrick123321123@icloud.com>
2022-09-18 11:39:32 +0000

    Create and test all models in admin panel all worked

--- skateapp/models.py
+++ skateapp/models.py
@@ -1,3 +1,46 @@
-from django.db import models
+# All necessary imports
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from cloudinary.models import CloudinaryField
+
+
+# All app models.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField('image')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f'{self.user.username} Comment'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image')
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+
+class PostTutorial(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    image = CloudinaryField('image')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
